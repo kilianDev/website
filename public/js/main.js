fa7f0898e5e95a3d642c3e341796ed37e5d2b853
kdevouassoux <kilian.devouassoux@gmail.com>
2015-12-05 19:10:27 +0100

    Going to prod kiliandevouassoux.fr

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  // catch scroll
   $(document).on("scroll", onScroll);
 
   //smoothscroll
@@ -23,7 +24,14 @@ $(document).ready(function() {
       });
   });
 
-  /* SKILL CHARTS */
+  // Collapse navbar on click, when in mobile view
+  $(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+  });
+
+  // Skills charts
   Chart.defaults.global.showTooltips = false;
   Chart.defaults.global.animation = false;
   Chart.defaults.global.responsive = true;
@@ -42,15 +50,30 @@ $(document).ready(function() {
   Chart.defaults.Radar.scaleBeginAtZero = true;
 
   var technicalSkills = [];
-  technicalSkills[1] = {label:"Scrum", value:4 };
-  technicalSkills[2] = {label:"PHP", value:3.5 }
-  technicalSkills[3] = {label:"MySQL", value:4 }
-  technicalSkills[4] = {label:"Système Linux", value:4 }
-  technicalSkills[5] = {label:"Réseaux", value:3.5 }
-  technicalSkills[6] = {label:"HTML/CSS", value:3 }
-  technicalSkills[7] = {label:"Javascript", value:3 }
-  technicalSkills[8] = {label:"Middleware", value:4 }
-  technicalSkills[9] = {label:"Scripting Bash/Perl", value:3.5 }
+  // Technique
+  technicalSkills.push({label:"PHP", value:3.5 });
+  technicalSkills.push({label:"MySQL", value:4 });
+  technicalSkills.push({label:"HTML/CSS", value:3 });
+  technicalSkills.push({label:"JavaScript", value:3 });
+  technicalSkills.push({label:"PhoneGap", value:1.5 });
+  //Système
+  technicalSkills.push({label:"Système Linux", value:4 });
+  technicalSkills.push({label:"Scripting Bash/Perl", value:3.5 });
+  technicalSkills.push({label:"Réseaux", value:4 });
+  technicalSkills.push({label:"Middleware", value:4 });
+  // Gestion de projets
+  technicalSkills.push({label:"Scrum", value:4 });
+
+  var professionalSkills = [];
+  professionalSkills.push({label:"Gestion du stress", value:4.5 });
+  professionalSkills.push({label:"Organisation", value:4.5 });
+  professionalSkills.push({label:"Autonomie", value:3 });
+  professionalSkills.push({label:"Travail en équipe", value:4 });
+  professionalSkills.push({label:"Prise de responsabilité", value:4 });
+  professionalSkills.push({label:"Leadership", value:4 });
+  professionalSkills.push({label:"Aisance en public", value:3 });
+  professionalSkills.push({label:"Patience", value:2.5 });
+  professionalSkills.push({label:"Coffee addict", value:5 });
 
   var technicalSkillsLabel = [];
   var technicalSkillsValue = [];
@@ -71,18 +94,6 @@ $(document).ready(function() {
       ]
   };
   var myTechnicalChart = new Chart(technicalCanvas).Radar(technicalData);
-
-
-  var professionalSkills = [];
-  professionalSkills[1] = {label:"Gestion du stress", value:4.5 };
-  professionalSkills[2] = {label:"Organisation", value:4.5 }
-  professionalSkills[3] = {label:"Travail en équipe", value:4 }
-  professionalSkills[4] = {label:"Prise de responsabilité", value:4 }
-  professionalSkills[5] = {label:"Autonomie", value:2.5 }
-  professionalSkills[6] = {label:"Leadership", value:4 }
-  professionalSkills[7] = {label:"Aisance en public", value:3 }
-  professionalSkills[8] = {label:"Patience", value:2.5 }
-  professionalSkills[9] = {label:"Faiseur de café", value:4 }
 
   var professionalSkillsLabel = [];
   var professionalSkillsValue = [];
@@ -106,9 +117,6 @@ $(document).ready(function() {
 
 });
 
-$.fn.scrollBottom = function() {
-  return $(document).height() - this.scrollTop() - this.height();
-};
 
 function onScroll(event){
   var scrollPos = $(document).scrollTop();
@@ -131,35 +139,3 @@ function onScroll(event){
       }
   });
 }
-
-/*
- * Add the ripple effect on navbar links
- */
-var parent, ink, d, x, y;
-
-$("ul li a").click(function(e){
-  parent = $(this).parent();
-  //create .ink element if it doesn't exist
-  if(parent.find(".ink").length == 0)
-    parent.prepend("<span class='ink'></span>");
-
-  ink = parent.find(".ink");
-  //incase of quick double clicks stop the previous animation
-  ink.removeClass("animate");
-
-  //set size of .ink
-  if(!ink.height() && !ink.width())
-  {
-    //use parent's width or height whichever is larger for the diameter to make a circle which can cover the entire element.
-    d = Math.max(parent.outerWidth(), parent.outerHeight());
-    ink.css({height: d, width: d});
-  }
-
-  //get click coordinates
-  //logic = click coordinates relative to page - parent's position relative to page - half of self height/width to make it controllable from the center;
-  x = e.pageX - parent.offset().left - ink.width()/2;
-  y = e.pageY - parent.offset().top - ink.height()/2;
-
-  //set the position and add class .animate
-  ink.css({top: y+'px', left: x+'px'}).addClass("animate");
-});
